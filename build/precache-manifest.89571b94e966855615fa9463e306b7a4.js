4dff27b38367ba977dc847098c241a3ee775c983
Rickey07 <Rickeyrickey747@gmail.com>
2020-06-24 14:19:06 +0530

    Build Added

---
+++ build/precache-manifest.89571b94e966855615fa9463e306b7a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a89e18613bdc44346ef",
+    "url": "/static/css/main.46e63d57.chunk.css"
+  },
+  {
+    "revision": "0a89e18613bdc44346ef",
+    "url": "/static/js/main.dc7ef219.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ab55d335f7712bd6094",
+    "url": "/static/js/2.a0f6b0b1.chunk.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  },
+  {
+    "revision": "39329f49397a37dfb8b06c32252887c7",
+    "url": "/index.html"
+  }
+];
